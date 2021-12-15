e5ed4759a6b81fa0a3a18c3e833818ad1f76472d
patrick.ddung2 <patrick.ddung2@kakaocorp.com>
2021-12-15 19:36:42 +0900

    게시글 작성

---
+++ src/test/java/com/lsh/springboot/web/IndexControllerTest.java
@@ -0,0 +1,25 @@
+package com.lsh.springboot.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩() {
+        String body = this.restTemplate.getForObject("/", String.class);
+        assertThat(body).contains("웹 서비스 시작");
+    }
+}
